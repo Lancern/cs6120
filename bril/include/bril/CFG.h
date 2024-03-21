565f1b2ee9df3492eacb15e3ba73ab55d25b6b36
Sirui Mu <msrlancern@gmail.com>
2024-03-21 23:29:05 +0800

    feat: add basic definition for CFGs

---
+++ bril/include/bril/CFG.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <vector>
+
+#include <boost/intrusive/list.hpp>
+#include <boost/intrusive/list_hook.hpp>
+#include <boost/intrusive/options.hpp>
+
+#include "Inst.h"
+
+namespace il = boost::intrusive;
+
+namespace bril {
+
+class CFG;
+class Function;
+class Inst;
+
+class BlockInstTag;
+class CFGBlockTag;
+
+/// A basic block within a CFG.
+///
+/// A basic block owns all instructions that are added to the block.
+class Block : il::list_base_hook<il::tag<CFGBlockTag>> {
+public:
+  Block(const Block &) = delete;
+  Block(Block &&) = delete;
+
+  ~Block() noexcept;
+
+  Block &operator=(const Block &) = delete;
+  Block &operator=(Block &&) = delete;
+
+  /// Get the CFG this block has been added to.
+  CFG *getCFG() const noexcept { return cfg_; }
+
+  /// Get the function this block has been added to.
+  Function *getFunction() const noexcept;
+
+  friend class Inst;
+
+private:
+  CFG *cfg_{nullptr};
+  il::list<Inst, il::base_hook<il::list_base_hook<il::tag<BlockInstTag>>>> inst_;
+  std::vector<Block *> succ_;
+  std::vector<Block *> pred_;
+};
+
+/// A control flow graph (CFG).
+class CFG {
+public:
+  /// Construct a new CFG object.
+  explicit CFG(Function *func) noexcept : func_{func} {}
+
+  CFG(const CFG &) = delete;
+  CFG(CFG &&) = delete;
+
+  ~CFG() noexcept;
+
+  CFG &operator=(const CFG &) = delete;
+  CFG &operator=(CFG &&) = delete;
+
+  /// Get the function that this CFG belongs to.
+  Function *getFunction() const noexcept { return func_; }
+
+private:
+  Function *func_;
+  il::list<Block, il::base_hook<il::list_base_hook<il::tag<CFGBlockTag>>>> blocks_;
+};
+
+} // namespace bril
---
+++ bril/src/CFG.cpp
@@ -0,0 +1,28 @@
+#include "bril/CFG.h"
+
+namespace bril {
+
+Block::~Block() noexcept {
+  while (!inst_.empty()) {
+    auto *i = &inst_.front();
+    inst_.pop_front();
+    delete i;
+  }
+}
+
+Function *Block::getFunction() const noexcept {
+  if (!cfg_) {
+    return nullptr;
+  }
+  return cfg_->getFunction();
+}
+
+CFG::~CFG() noexcept {
+  while (!blocks_.empty()) {
+    auto *b = &blocks_.front();
+    blocks_.pop_front();
+    delete b;
+  }
+}
+
+}  // namespace bril
